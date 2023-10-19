ba572adba4ec281b90493cebfda8de364c7d7f78
mariamawit <mariamawitashenafi@yahoo.fr>
2023-10-19 13:22:54 +0300

    fix: changed nav label, color palette

--- src/navBar.js
+++ src/navBar.js
@@ -7,8 +7,8 @@ const NavBar = () => {
             <Link style={currentRoute==="/"? {backgroundColor: 'var(--color-yellow)', height: '100%'}:{}} className="nav_link" id="left_outer_link" to="/">Home</Link>
             {/* <Link style={currentRoute.includes("about")? {backgroundColor: 'var(--color-yellow)'}:{}} className="nav_link" to="/about">About Me</Link> */}
             <Link style={currentRoute.includes("skills")? {backgroundColor: 'var(--color-yellow)'}:{}} className="nav_link inner_nav" to="/skills">Skills</Link>
-            <Link style={currentRoute.includes("experience")? {backgroundColor: 'var(--color-yellow)'}:{}} className="nav_link inner_nav" to="/experience">Experience</Link>
-            <Link style={currentRoute.includes("contact")? {backgroundColor: 'var(--color-yellow)'}:{}} className="nav_link" id="right_outer_link" to="/contact">Get In Touch</Link>
+            <Link style={currentRoute.includes("experience")? {backgroundColor: 'var(--color-yellow)'}:{}} className="nav_link inner_nav" to="/experience">Projects</Link>
+            <Link style={currentRoute.includes("contact")? {backgroundColor: 'var(--color-yellow)'}:{}} className="nav_link" id="right_outer_link" to="/contact">Contact</Link>
         </nav>
     );
 }
