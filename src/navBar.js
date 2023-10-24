ca37c5fb3cc8c08e0a7ea87e4b06ba9d39149ff0
mariamawit <mariamawitashenafi@yahoo.fr>
2023-10-25 00:08:54 +0300

    fix: logo

--- src/navBar.js
+++ src/navBar.js
@@ -5,7 +5,6 @@ const NavBar = () => {
     return(
         <nav className="nav_bar" id="menu">
             <Link style={currentRoute==="/"? {backgroundColor: 'var(--color-yellow)', height: '100%'}:{}} className="nav_link" id="left_outer_link" to="/">Home</Link>
-            {/* <Link style={currentRoute.includes("about")? {backgroundColor: 'var(--color-yellow)'}:{}} className="nav_link" to="/about">About Me</Link> */}
             <Link style={currentRoute.includes("skills")? {backgroundColor: 'var(--color-yellow)'}:{}} className="nav_link inner_nav" to="/skills">Skills</Link>
             <Link style={currentRoute.includes("projects")? {backgroundColor: 'var(--color-yellow)'}:{}} className="nav_link inner_nav" to="/projects">Projects</Link>
             <Link style={currentRoute.includes("contact")? {backgroundColor: 'var(--color-yellow)'}:{}} className="nav_link" id="right_outer_link" to="/contact">Contact</Link>
